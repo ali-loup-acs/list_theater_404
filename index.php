794b4778fe2aca715ebc4546a4ec172c8662a7c4
julke <julestodeschini@gmail.com>
2017-01-18 15:53:45 +0100

    MAJ de la map
    mise à jour de la carte de france

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
-<?php
-  echo 'start';
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>list_theater</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script type="text/javascript" src="cmap/france.js"></script>
+<script type="text/javascript" src="cmap/cmap.js"></script>
+</head>
 
- ?>
+<body>
+<h2>Nouvelles régions de France</h2>
+
+<div id="tip">&nbsp;</div>
+<div id="cmap"></div>
+<script type="text/javascript" src="cmap/france.js"></script>
+<script type="text/javascript" src="cmap/cmap.js"></script>
+<!-- fin du code -->
+<ol>
+<div id="tip">&nbsp;</div>
+<div id="cmap"></div>
+<script type="text/javascript" src="cmap/france.js"></script>
+<script type="text/javascript" src="cmap/cmap.js"></script>
+</textarea>
+</li>
+</ol>
+
+
+
+
+
+</body>
+</html>
