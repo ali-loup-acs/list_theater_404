6951510e6f495edfb73f7b8a9ac5b7ed7ca8b9d0
Ali DJEMAÏ <ajemai@users.noreply.github.com>
2017-03-08 13:35:23 +0100

    tag missing list_spectacles + fixing false results page list_spectacles

--- list_spectacles.php
+++ list_spectacles.php
@@ -2,26 +2,26 @@
 require_once('include/require.php');
 require_once('include/lib/IteratorPresenter.class.php');
 define('NB_BY_PAGE',6);
-define('MAX_PAGER',9);
+define('MAX_PAGER',5);
 
 $spectacles = new Spectacles();
 //update database
 $spectacles->update_spectacles();
 
-$zip_code =  '';
+$zip_code =  0;
 $offset = 0;
 $nb_by_page = 6;
 $spectacles_result = array();
 
 if(isset($_REQUEST['zip_code'])){
-  $zip_code = $_REQUEST['zip_code'];
+  $zip_code = intval($_REQUEST['zip_code']);
 }
 
 if (isset($_REQUEST['offset'])){
   $offset = $_REQUEST['offset'];
 }
 
-if ($spectacles->is_valid_zip_code($zip_code)) {
+if($spectacles->is_valid_zip_code($zip_code)) {
   $spectacles_result['spectacles'] = $spectacles->find_spectacles_by_zip_code($zip_code,NB_BY_PAGE,$offset);
 } else {
   $spectacles->limitData(NB_BY_PAGE,$offset);
@@ -29,6 +29,8 @@ if ($spectacles->is_valid_zip_code($zip_code)) {
 }
 
 $spectacles_result['pages'] = new IteratorPresenter($spectacles->pagerData(MAX_PAGER)['pages']); // reformat the numeric array to an associative array
+// print_r($spectacles);
+// exit();
 $spectacles_result['zip_code'] = $zip_code;
 
 render_template('list_spectacles',$spectacles_result);
